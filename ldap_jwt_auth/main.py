91a6c16a03fd028c27e5193ea277a26bf05c5835
VKTB <45173816+VKTB@users.noreply.github.com>
2023-12-18 13:06:33 +0000

    Configure pylint #1

--- ldap_jwt_auth/main.py
+++ ldap_jwt_auth/main.py
@@ -1,3 +1,6 @@
+"""
+Main module contains the API entrypoint.
+"""
 import logging
 
 from fastapi import FastAPI
@@ -16,4 +19,7 @@ logger.info("Logging now setup")
 
 @app.get("/")
 def read_root():
+    """
+    Root endpoint for the API.
+    """
     return {"title": API_TITLE}
