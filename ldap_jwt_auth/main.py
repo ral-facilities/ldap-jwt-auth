b943452815d5f0c8475a0b044a8189b5642901c9
VKTB <45173816+VKTB@users.noreply.github.com>
2023-12-18 13:40:58 +0000

    Fix API CORS issues #1

--- ldap_jwt_auth/main.py
+++ ldap_jwt_auth/main.py
@@ -4,6 +4,7 @@ Main module contains the API entrypoint.
 import logging
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from ldap_jwt_auth.core.logger_setup import setup_logger
 
@@ -15,6 +16,17 @@ app = FastAPI(title=API_TITLE, description=API_DESCRIPTION)
 setup_logger()
 logger = logging.getLogger()
 logger.info("Logging now setup")
+
+# Fixes CORS issues but should be updated before deploying to prod
+ALLOWED_ORIGINS = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
