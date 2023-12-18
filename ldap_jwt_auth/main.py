9990644ce07bfd981f86c109f27a518f72345ddd
VKTB <45173816+VKTB@users.noreply.github.com>
2023-12-18 12:47:51 +0000

    Set up logging #1

--- ldap_jwt_auth/main.py
+++ ldap_jwt_auth/main.py
@@ -1,9 +1,17 @@
+import logging
+
 from fastapi import FastAPI
+
+from ldap_jwt_auth.core.logger_setup import setup_logger
 
 API_DESCRIPTION = "This is the API for the LDAP-JWT Authentication Service"
 API_TITLE = "LDAP-JWT Authentication Service API"
 
 app = FastAPI(title=API_TITLE, description=API_DESCRIPTION)
+
+setup_logger()
+logger = logging.getLogger()
+logger.info("Logging now setup")
 
 
 @app.get("/")
