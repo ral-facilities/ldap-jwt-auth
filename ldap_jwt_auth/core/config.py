dae3eff913ee99709818c19abdad214d3e92c8a4
VKTB <45173816+VKTB@users.noreply.github.com>
2023-12-20 09:56:13 +0000

    Define a configuration model for the LDAP server #4

--- ldap_jwt_auth/core/config.py
+++ ldap_jwt_auth/core/config.py
@@ -27,10 +27,27 @@ class AuthenticationConfig(BaseModel):
     refresh_token_validity_days: int
 
 
+class LDAPServerConfig(BaseModel):
+    """
+    Configuration model for the LDAP server.
+    """
+
+    url: str
+    realm: str
+
+
 class Config(BaseSettings):
+    """
+    Overall configuration model for the application.
+
+    It includes attributes for the API, authentication, and LDAP server configurations. The class inherits from
+    `BaseSettings` and automatically reads environment variables. If values are not passed in form of system environment
+    variables at runtime, it will attempt to read them from the .env file.
+    """
 
     api: APIConfig
     authentication: AuthenticationConfig
+    ldap_server: LDAPServerConfig
 
 
 config = Config()
