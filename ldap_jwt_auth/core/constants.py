699badf505849955c4be9a6dd1ed48bd5427a927
VKTB <45173816+VKTB@users.noreply.github.com>
2023-12-20 10:22:54 +0000

    Use utf-8 encoding for the open method #4

--- ldap_jwt_auth/core/constants.py
+++ ldap_jwt_auth/core/constants.py
@@ -8,13 +8,13 @@ from ldap_jwt_auth.core.config import config
 # Read the contents of the private and public key files into constants. These are used for encoding and decoding of JWT
 # access and refresh tokens.
 try:
-    with open(config.authentication.private_key_path, "r") as f:
+    with open(config.authentication.private_key_path, "r", encoding="utf-8") as f:
         PRIVATE_KEY = f.read()
 except FileNotFoundError as exc:
     sys.exit(f"Cannot find private key: {exc}")
 
 try:
-    with open(config.authentication.public_key_path, "r") as f:
+    with open(config.authentication.public_key_path, "r", encoding="utf-8") as f:
         PUBLIC_KEY = f.read()
 except FileNotFoundError as exc:
     sys.exit(f"Cannot find public key: {exc}")
