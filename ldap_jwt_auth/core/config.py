e65da3839c90d1784e0fd57dc78df1df59260637
VKTB <45173816+VKTB@users.noreply.github.com>
2023-12-20 09:55:40 +0000

    Define a configuration model for the Authentication #4

--- ldap_jwt_auth/core/config.py
+++ ldap_jwt_auth/core/config.py
@@ -15,9 +15,22 @@ class APIConfig(BaseModel):
     root_path: str = ""  # (If using a proxy) The path prefix handled by a proxy that is not seen by the app.
 
 
+class AuthenticationConfig(BaseModel):
+    """
+    Configuration model for the authentication.
+    """
+
+    private_key_path: str
+    public_key_path: str
+    jwt_algorithm: str
+    access_token_validity_minutes: int
+    refresh_token_validity_days: int
+
+
 class Config(BaseSettings):
 
     api: APIConfig
+    authentication: AuthenticationConfig
 
 
 config = Config()
