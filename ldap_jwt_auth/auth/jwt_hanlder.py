4aedd563e2bcc41c3afdd9649ceaa6cf344f4e51
VKTB <45173816+VKTB@users.noreply.github.com>
2023-12-21 08:02:57 +0000

    Define method for packing and signing JWTs #6

--- ldap_jwt_auth/auth/jwt_hanlder.py
+++ ldap_jwt_auth/auth/jwt_hanlder.py
@@ -3,6 +3,12 @@ Module for providing a class for handling JWTs.
 """
 import logging
 
+import jwt
+from cryptography.hazmat.primitives import serialization
+
+from ldap_jwt_auth.core.config import config
+from ldap_jwt_auth.core.constants import PRIVATE_KEY
+
 logger = logging.getLogger()
 
 
@@ -10,3 +16,15 @@ class JWTHandler:
     """
     Class for handling JWTs.
     """
+
+    def _pack_jwt(self, payload: dict) -> str:
+        """
+        Packs the provided payload into a JWT token and signs it.
+        :param payload: The payload to be packed.
+        :return: The encoded and signed JWT.
+        """
+        logger.debug("Packing payload into a JWT")
+        bytes_key = bytes(PRIVATE_KEY, encoding="utf8")
+        loaded_private_key = serialization.load_ssh_private_key(bytes_key, password=None)
+        token = jwt.encode(payload, loaded_private_key, algorithm=config.authentication.jwt_algorithm)
+        return token
