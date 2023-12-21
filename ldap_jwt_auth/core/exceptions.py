cd8b0ca50e179f2a8a99ed1919f8cab5d4b2c3c2
VKTB <45173816+VKTB@users.noreply.github.com>
2023-12-21 08:00:37 +0000

    Define InvalidCredentialsError custom exception class #6

--- ldap_jwt_auth/core/exceptions.py
+++ ldap_jwt_auth/core/exceptions.py
@@ -7,3 +7,9 @@ class LDAPServerError(Exception):
     """
     Exception raised when there is problem with the LDAP server.
     """
+
+
+class InvalidCredentialsError(Exception):
+    """
+    Exception raised when invalid credentials are provided.
+    """
