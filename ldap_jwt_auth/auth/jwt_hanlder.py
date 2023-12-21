5f4f7fe79a9cb8cfd07c09a7166096070f55c750
VKTB <45173816+VKTB@users.noreply.github.com>
2023-12-21 15:11:33 +0000

    Define method for decoding and getting JWT payload #8

--- ldap_jwt_auth/auth/jwt_hanlder.py
+++ ldap_jwt_auth/auth/jwt_hanlder.py
@@ -3,12 +3,13 @@ Module for providing a class for handling JWTs.
 """
 import logging
 from datetime import datetime, timezone, timedelta
+from typing import Any, Dict
 
 import jwt
 from cryptography.hazmat.primitives import serialization
 
 from ldap_jwt_auth.core.config import config
-from ldap_jwt_auth.core.constants import PRIVATE_KEY
+from ldap_jwt_auth.core.constants import PRIVATE_KEY, PUBLIC_KEY
 
 logger = logging.getLogger()
 
@@ -42,14 +43,25 @@ class JWTHandler:
         }
         return self._pack_jwt(payload)
 
+    def _get_jwt_payload(self, token: str, jwt_decode_options: dict | None = None) -> Dict[str, Any]:
+        """
+        Decodes the provided JWT token and gets its payload.
+        :param token: The JWT token to decode and get payload from.
+        :param jwt_decode_options: Any options to be passed to the `decode` method.
+        :return: Payload from the provided JWT token.
+        """
+        logger.info("Decoding JWT token")
+        return jwt.decode(
+            token, PUBLIC_KEY, algorithms=[config.authentication.jwt_algorithm], options=jwt_decode_options
+        )
+
     def _pack_jwt(self, payload: dict) -> str:
         """
         Packs the provided payload into a JWT token and signs it.
         :param payload: The payload to be packed.
-        :return: The encoded and signed JWT.
+        :return: The encoded and signed JWT token.
         """
-        logger.debug("Packing payload into a JWT")
+        logger.debug("Packing payload into a JWT token")
         bytes_key = bytes(PRIVATE_KEY, encoding="utf8")
         loaded_private_key = serialization.load_ssh_private_key(bytes_key, password=None)
-        token = jwt.encode(payload, loaded_private_key, algorithm=config.authentication.jwt_algorithm)
-        return token
+        return jwt.encode(payload, loaded_private_key, algorithm=config.authentication.jwt_algorithm)
