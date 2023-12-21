2f81d87054c18382e0894eddbbfb97c1e1d3d72e
VKTB <45173816+VKTB@users.noreply.github.com>
2023-12-21 08:06:40 +0000

    Define class for managing authentication #6

---
+++ ldap_jwt_auth/auth/authentication.py
@@ -0,0 +1,12 @@
+"""
+Module for providing a class for managing authentication.
+"""
+import logging
+
+logger = logging.getLogger()
+
+
+class Authentication:
+    """
+    Class for managing authentication against an LDAP server.
+    """
