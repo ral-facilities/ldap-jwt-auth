65e2534fab7ae3eff16d9c322a8a14f9ab4059b9
VKTB <45173816+VKTB@users.noreply.github.com>
2024-03-01 13:25:37 +0000

    Make allowed CORS origins configurable #47

--- ldap_jwt_auth/core/config.py
+++ ldap_jwt_auth/core/config.py
@@ -3,6 +3,7 @@ Module for the overall configuration for the application.
 """
 
 from pathlib import Path
+from typing import List
 
 from pydantic import BaseModel
 from pydantic_settings import SettingsConfigDict, BaseSettings
@@ -16,6 +17,7 @@ class APIConfig(BaseModel):
     title: str = "LDAP-JWT Authentication Service API"
     description: str = "This is the API for the LDAP-JWT Authentication Service"
     root_path: str = ""  # (If using a proxy) The path prefix handled by a proxy that is not seen by the app.
+    allowed_cors_origins: List[str]
 
 
 class AuthenticationConfig(BaseModel):
--- ldap_jwt_auth/main.py
+++ ldap_jwt_auth/main.py
@@ -52,12 +52,9 @@ async def custom_validation_exception_handler(request: Request, exc: RequestVali
     return await request_validation_exception_handler(request, exc)
 
 
-# Fixes CORS issues but should be updated before deploying to prod
-ALLOWED_ORIGINS = ["*"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins=config.api.allowed_cors_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
