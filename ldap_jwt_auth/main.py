f49eec4e32dac411b24682afc51874042da4c518
VKTB <45173816+VKTB@users.noreply.github.com>
2024-03-01 13:27:00 +0000

    Make allowed CORS headers configurable #47

--- ldap_jwt_auth/main.py
+++ ldap_jwt_auth/main.py
@@ -57,7 +57,7 @@ app.add_middleware(
     allow_origins=config.api.allowed_cors_origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=config.api.allowed_cors_headers,
 )
 
 app.include_router(login.router)
