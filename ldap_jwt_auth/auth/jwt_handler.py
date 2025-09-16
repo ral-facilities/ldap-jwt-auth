849b729fe60a214519e9e12e76310c7fd231fab0
VKTB <45173816+VKTB@users.noreply.github.com>
2025-09-16 16:24:39 +0000

    Update refresh_access_token method logic accordingly #245

--- ldap_jwt_auth/auth/jwt_handler.py
+++ ldap_jwt_auth/auth/jwt_handler.py
@@ -9,7 +9,7 @@ from typing import Any, Dict
 import jwt
 from cryptography.hazmat.primitives import serialization
 
-from ldap_jwt_auth.auth.authentication import Authentication
+from ldap_jwt_auth.auth.authentication import LDAPAuthentication, OIDCAuthentication
 from ldap_jwt_auth.core.config import config
 from ldap_jwt_auth.core.constants import PRIVATE_KEY, PUBLIC_KEY
 from ldap_jwt_auth.core.exceptions import InvalidJWTError, JWTRefreshError, UserNotActiveError, UsernameMismatchError
@@ -25,6 +25,7 @@ class JWTHandler:
     def get_access_token(self, username: str) -> str:
         """
         Generates a payload and returns a signed JWT access token.
+
         :param username: The username of the user.
         :return: The signed JWT access token
         """
@@ -38,6 +39,7 @@ class JWTHandler:
     def get_refresh_token(self, username: str) -> str:
         """
         Generates a payload and returns a signed JWT refresh token.
+
         :param username: The username of the user.
         :return: The signed JWT refresh token.
         """
@@ -48,12 +50,13 @@ class JWTHandler:
         }
         return self._pack_jwt(payload)
 
-    def refresh_access_token(self, access_token: str, refresh_token: str):
+    def refresh_access_token(self, access_token: str, refresh_token: str) -> str:
         """
         Refreshes the JWT access token by updating its expiry time, provided that the JWT refresh token is valid.
 
         Before attempting to refresh the token, it checks that the usernames in the access and refresh tokens match, and
         that the username is still part of the active usernames.
+
         :param access_token: The JWT access token to refresh.
         :param refresh_token: The JWT refresh token.
         :raises JWTRefreshError: If the JWT access token cannot be refreshed.
@@ -66,14 +69,12 @@ class JWTHandler:
 
         try:
             access_token_payload = self._get_jwt_payload(access_token, {"verify_exp": False})
-
-            authentication = Authentication()
             username = access_token_payload["username"]
 
             if username != refresh_token_payload["username"]:
                 raise UsernameMismatchError("The usernames in the access and refresh tokens do not match")
 
-            if not authentication.is_user_active(username):
+            if not self._is_user_active(username):
                 raise UserNotActiveError(f"The provided username '{username}' is not part of the active usernames")
 
             access_token_payload["exp"] = datetime.now(timezone.utc) + timedelta(
@@ -89,6 +90,7 @@ class JWTHandler:
         """
         Verifies that the provided JWT token is valid. It does this by checking that it was signed by the corresponding
         private key and has not expired.
+
         :param token: The JWT token to be verified.
         :raises InvalidJWTError: If the JWT token is invalid.
         :return: The payload of the verified JWT token.
@@ -101,9 +103,27 @@ class JWTHandler:
             logger.exception(message)
             raise InvalidJWTError(message) from exc
 
+    def _is_user_active(self, username: str) -> bool:
+        """
+        Checks if the provided username is active. If the username contains an '@' and '.' charcaters then it should be
+        checked against the list of OIDC active user emails as the user was authenticated through OIDC. If this is not
+        the case then it should be checked against the list of LPDA active usernames.
+
+        :param username: The username to check.
+        :return: `True` if the user is active, `False` otherwise.  
+        """
+
+        if "@" in username and "." in username:
+            oidc_authentication = OIDCAuthentication()
+            return oidc_authentication.is_user_active(username)
+
+        ldap_authentication = LDAPAuthentication()
+        return ldap_authentication.is_user_active(username)
+
     def _get_jwt_payload(self, token: str, jwt_decode_options: dict | None = None) -> Dict[str, Any]:
         """
         Decodes the provided JWT token and gets its payload.
+
         :param token: The JWT token to decode and get payload from.
         :param jwt_decode_options: Any options to be passed to the `decode` method.
         :return: Payload from the provided JWT token.
@@ -116,6 +136,7 @@ class JWTHandler:
     def _pack_jwt(self, payload: dict) -> str:
         """
         Packs the provided payload into a JWT token and signs it.
+
         :param payload: The payload to be packed.
         :return: The encoded and signed JWT token.
         """
