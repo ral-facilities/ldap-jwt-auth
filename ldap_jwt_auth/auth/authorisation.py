b4d423a93b1849123896c708384cf17e4e5a5e29
MatteoGuarnaccia5 <matteo.guarnaccia@stfc.ac.uk>
2025-10-13 09:09:35 +0000

    addressed review comments #246

--- ldap_jwt_auth/auth/authorisation.py
+++ ldap_jwt_auth/auth/authorisation.py
@@ -64,11 +64,12 @@ class Authorisation:
         :param role: The role for the given user
         :return: `True` if the user has a role which matches the role(s) with the highest privilege, `False` otherwise
         """
+        
+        # protects against role with no dictionary defined
+        role_config = self.roles.get(role, None)
+        return role_config is not None and role_config.get("userIsAdmin", False) == True
+        
 
-        if self.roles.get(role, {}).get("userIsAdmin", False):
-            return True
-
-        return False
 
     def _find_user(self, identifier: str) -> dict | None:
         """
